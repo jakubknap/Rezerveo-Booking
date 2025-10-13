06f6ecb65ed2934785c97de4d3a253267551bb83
Jakub Knap <jknap40@gmail.com>
2025-10-13 16:33:30 +0200

    Initial commit

---
+++ src/main/java/pl/rezerveo/booking/RezerveoBookingApplication.java
@@ -0,0 +1,12 @@
+package pl.rezerveo.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerveoBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezerveoBookingApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/rezerveo/booking/RezerveoBookingApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.rezerveo.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezerveoBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
